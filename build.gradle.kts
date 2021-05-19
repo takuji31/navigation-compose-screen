b0f67faed8bb0dbd6ee29363e4a2538425f1460c
Takuji Nishibayashi <takuji31@gmail.com>
2021-05-19 15:43:10 +0900

    Update Gradle Plugin to 7.0.0-beta01

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha14")
+        classpath("com.android.tools.build:gradle:7.0.0-beta01")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
         classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.hiltVersion}")
     }
