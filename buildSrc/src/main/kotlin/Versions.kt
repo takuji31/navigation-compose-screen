1436c0f21937867fdc6d71ee8043a7097abfb20e
Takuji Nishibayashi <takuji31@gmail.com>
2021-07-19 12:54:32 +0900

    Update androidx.navigation to 2.4.0-alpha02

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,5 +2,5 @@ object Versions {
     const val composeVersion = "1.0.0-rc02"
     const val lifecycleVersion = "2.3.0"
     const val hiltVersion = "2.35"
-    const val navigationVersion = "2.4.0-alpha02"
+    const val navigationVersion = "2.4.0-alpha04"
 }
