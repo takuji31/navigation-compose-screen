1a5b97b9118637dbe06e6f0bd2c6f9f3c3f63627
Takuji Nishibayashi <takuji31@gmail.com>
2021-07-19 12:54:17 +0900

    Update Jetpack Compose to 1.0.0-rc02

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val composeVersion = "1.0.0-beta08"
+    const val composeVersion = "1.0.0-rc02"
     const val lifecycleVersion = "2.3.0"
     const val hiltVersion = "2.35"
     const val navigationVersion = "2.4.0-alpha02"
