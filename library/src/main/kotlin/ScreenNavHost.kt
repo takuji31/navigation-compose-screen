670d0c15b9847ea9112daf24559cdd093b45a288
NUmeroAndDev <numero.dev.and@gmail.com>
2021-09-01 13:30:27 +0900

    Added Modifier argument to ScreenNavHost

--- library/src/main/kotlin/ScreenNavHost.kt
+++ library/src/main/kotlin/ScreenNavHost.kt
@@ -1,6 +1,7 @@
 package jp.takuji31.compose.navigation.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 
@@ -9,10 +10,12 @@ fun ScreenNavHost(
     navController: ScreenNavController,
     startScreen: Screen<*>,
     builder: NavGraphBuilder.() -> Unit,
+    modifier: Modifier = Modifier,
 ) {
     NavHost(
         navController = navController.navController,
         startDestination = startScreen.parameterizedRoute,
         builder = builder,
+        modifier = modifier,
     )
 }
