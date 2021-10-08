1becf9985fce13d16076bb706c564df6a85fdd00
Takuji Nishibayashi <takuji31@gmail.com>
2021-10-08 22:14:34 +0900

    Set kotlin jvm target

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,17 +1,15 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    id("java-library")
-    id("kotlin")
+    kotlin("jvm")
+    `java-library`
     `maven-publish`
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
 
-dependencies {
-
-}
 publishing {
     publications {
         create<MavenPublication>("release") {
--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -1,13 +1,14 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    id("java-library")
-    id("kotlin")
+    kotlin("jvm")
+    `java-library`
     kotlin("kapt")
     `maven-publish`
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 dependencies {
