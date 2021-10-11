366cddcb4a9bf4b9d616a8b99b727dd4e44d747e
Takuji Nishibayashi <takuji31@gmail.com>
2021-10-11 10:34:01 +0900

    Update Navigation Compose to 2.4.0-alpha10

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,5 +3,5 @@ object Versions {
     const val composeVersion = "1.0.3"
     const val lifecycleVersion = "2.3.0"
     const val hiltVersion = "2.38.1"
-    const val navigationVersion = "2.4.0-alpha09"
+    const val navigationVersion = "2.4.0-alpha10"
 }
